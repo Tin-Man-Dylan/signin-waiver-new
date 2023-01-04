256a09eb3b6d0119ed4e5272dcd85fde5c9be041
Tin-Man-Dylan <tinman.dylan@gmail.com>
2023-01-03 19:18:05 -0700

    Added PDF generation with jsPDF. Next I will use capacitor to build it into an app

--- src/app/waiver-text/waiver-text.component.ts
+++ src/app/waiver-text/waiver-text.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import * as moment from 'moment';
-/*import { jsPDF } from 'jspdf';
-import html2canvas from 'html2canvas';*/
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 
 @Component({
@@ -27,22 +27,22 @@ export class WaiverTextComponent implements OnInit {
   }
 
   generatePDF(){
-  /*	var pdfHeight = 210; //in mm
-   	var pdfWidth = 297;
+  	var pdfHeight = 297; //in mm
+   	var pdfWidth = 210;
 
-   	let data = document.getElementById('divToPrint');  
+   	let data = document.getElementById('divToPrint')!;  
         html2canvas(data, {
         scrollX: -7,
         scrollY: -window.scrollY,
         scale: 1,
       }).then(canvas => {
         const contentDataURL = canvas.toDataURL('image/png')  
-        let pdf = new jsPDF('l', 'mm',  [pdfWidth, pdfHeight]);
+        let pdf = new jsPDF('p', 'mm',  [pdfWidth, pdfHeight]);
         //let pdf = new jspdf('l', 'cm', 'a4'); //Generates PDF in landscape mode
         //let pdf = new jsPDF('p', 'cm', 'a4'); //Generates PDF in portrait mode
         pdf.addImage(contentDataURL, 'PNG', 0, 0, pdfWidth, pdfHeight);
         pdf.save(this.firstName+this.lastName+'_'+moment().format('YYYY-MM-DD')+'.pdf');
-     });*/
+     });
 
   }
 
