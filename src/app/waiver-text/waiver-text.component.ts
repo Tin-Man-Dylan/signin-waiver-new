3f75c5490fb8cb56dc7d7c0fc3004c7144e6c974
Tin-Man-Dylan <tinman.dylan@gmail.com>
2023-01-09 18:49:35 -0700

    PDF generation in app version now works by using Capacitor filesystem

--- src/app/waiver-text/waiver-text.component.ts
+++ src/app/waiver-text/waiver-text.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import * as moment from 'moment';
 import { jsPDF } from 'jspdf';
 import html2canvas from 'html2canvas';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 
 @Component({
@@ -41,9 +42,26 @@ export class WaiverTextComponent implements OnInit {
         //let pdf = new jspdf('l', 'cm', 'a4'); //Generates PDF in landscape mode
         //let pdf = new jsPDF('p', 'cm', 'a4'); //Generates PDF in portrait mode
         pdf.addImage(contentDataURL, 'PNG', 0, 0, pdfWidth, pdfHeight);
-        pdf.save(this.firstName+this.lastName+'_'+moment().format('YYYY-MM-DD')+'.pdf');
+        //pdf.save(this.firstName+this.lastName+'_'+moment().format('YYYY-MM-DD')+'.pdf');
+
+        let pdfOutput = pdf.output('datauri');
+        console.log("here out")
+        this.writePDF(pdfOutput)
+
+        
      });
 
   }
+
+      async writePDF(pdfOutputString: any) {
+          console.log("here in")
+          await Filesystem.appendFile({
+            path: this.firstName+this.lastName+'_'+moment().format('YYYY-MM-DD')+'.pdf',
+            data: pdfOutputString,
+            directory: Directory.Documents,
+            //encoding: Encoding.UTF8,
+          });
+          console.log("here done")
+        };
 
 }
